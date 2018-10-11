bfc04f80e83450390ad77529e72dd8cb99f34ff0
yeonnic <exqa1232@gmail.com>
2018-10-11 20:47:31 +0900

    new file:   Makefile 	new file:   README.md 	new file:   argx.hpp 	new file:   main.cpp 	new file:   tokenize.cpp 	new file:   tokenize.hpp

---
+++ README.md
@@ -0,0 +1,19 @@
+# solparser
+
+## Install
+```
+make install
+```
+
+## How to use
+```
+-h, --help                        Display this help menu
+
+read source options:
+  -f[NAME], --file=[NAME]           read the source file
+  -d, --stdin                       read the stdin
+
+
+$ ./solparser -f ./test.sol
+```
+# solparser
---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include "./tokenize.hpp"
+#include "./argx.hpp"
+
+string ReadFile(const string &fileName){
+  ifstream f(fileName);
+  string s;
+
+  if(f.is_open()){
+    f.seekg(0, ios::end);
+
+    size_t size = f.tellg();
+
+    s.resize(size);
+
+    f.seekg(0, ios::beg);
+
+    f.read(&s[0], size);
+  } else {
+    cout << "file open error!" << endl;
+    exit(-1);
+  }
+
+  return s;
+}
+
+void TokensView(const vector<Token> &T){
+  cout << "[ ";
+  for ( auto i: T){
+    cout << "'\x1b[91m" << i.str << "\x1b[0m', ";
+  }
+  cout << "]" << endl;
+}
+
+int main(int argc, char **argv){
+
+  args::ArgumentParser parser("This is a solidity parse program.", "This goes after the options.");
+  args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+
+  args::Group inputGroup(parser, "read source options:", args::Group::Validators::Xor);
+  args::ValueFlag<string> file(inputGroup, "NAME", "read the source file", {'f', "file"});
+  args::Flag sinput(inputGroup, "stdin", "read the stdin", {'d', "stdin"});
+
+  try {
+    parser.ParseCLI(argc ,argv);
+
+
+    if(file || sinput){
+      string source;
+
+      if(file){
+        source = ReadFile(args::get(file));
+
+        cout << source << endl;
+
+      } else if(sinput){
+        cout << "stdin input!!!" << endl;
+      }
+      
+      auto tokens = Tokenize(source);
+
+      TokensView(tokens);
+    }
+  } catch (args::Completion& e){
+    cout << e.what() << endl;
+    return 0;
+  } catch (const args::Help&){
+    cout << parser;
+    return 0;
+  } catch (const args::ParseError& e){
+    cerr << e.what() << std::endl;
+    cerr << parser;
+    return -1;
+  } catch (const args::ValidationError e){
+    std::cerr << parser;
+    return 1;
+  }
+  return 0;
+
+}
